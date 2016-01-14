cd04e8d74afa2eb817a448f1931fb997cab9a9d7
Ahmed Agabani <agabani@live.com>
2016-01-14 23:34:14 +0000

    Abstract Factory

---
+++ DesignPatterns.UnitTests/Creational/AbstractFactory/AbstractFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using DesignPatterns.Creational.AbstractFactory;
+using NUnit.Framework;
+
+namespace DesignPatterns.UnitTests.Creational.AbstractFactory
+{
+    [TestFixture]
+    public class AbstractFactoryTests
+    {
+        [Test]
+        public void FactoryTest()
+        {
+            Factory factory = new PcFactory();
+
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"vertically challenged\""));
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"factually inaccurate\""));
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"chronologically gifted\""));
+            Assert.That(factory.MakeCompromise().ToString(), Is.EqualTo("\"do it your way, any way, or no way\""));
+            Assert.That(factory.MakeGrade().ToString(), Is.EqualTo("\"you pass, self-esteem intact\""));
+
+            factory = new NotPcFactory();
+
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"short\""));
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"lie\""));
+            Assert.That(factory.MakePhrase().ToString(), Is.EqualTo("\"old\""));
+            Assert.That(factory.MakeCompromise().ToString(), Is.EqualTo("\"my way, or the highway\""));
+            Assert.That(factory.MakeGrade().ToString(), Is.EqualTo("\"take test, deal with the results\""));
+        }
+    }
+}
---
+++ DesignPatterns/Creational/AbstractFactory/Factory.cs
@@ -0,0 +1,52 @@
+﻿namespace DesignPatterns.Creational.AbstractFactory
+{
+    public abstract class Factory
+    {
+        protected Expression Prototype;
+
+        public Expression MakePhrase()
+        {
+            return Prototype.Clone();
+        }
+
+        public abstract Expression MakeCompromise();
+
+        public abstract Expression MakeGrade();
+    }
+
+    public class PcFactory : Factory
+    {
+        public PcFactory()
+        {
+            Prototype = new PcPhrase();
+        }
+
+        public override Expression MakeCompromise()
+        {
+            return new Expression("\"do it your way, any way, or no way\"");
+        }
+
+        public override Expression MakeGrade()
+        {
+            return new Expression("\"you pass, self-esteem intact\"");
+        }
+    }
+
+    public class NotPcFactory : Factory
+    {
+        public NotPcFactory()
+        {
+            Prototype = new NotPcPhrase();
+        }
+
+        public override Expression MakeCompromise()
+        {
+            return new Expression("\"my way, or the highway\"");
+        }
+
+        public override Expression MakeGrade()
+        {
+            return new Expression("\"take test, deal with the results\"");
+        }
+    }
+}
