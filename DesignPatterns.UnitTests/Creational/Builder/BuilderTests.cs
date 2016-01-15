5f9502543cc70458ca99e7e63a3083b96fe8632e
Ahmed Agabani <agabani@live.com>
2016-01-15 02:15:20 +0000

    Builder

---
+++ DesignPatterns.UnitTests/Creational/Builder/BuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using DesignPatterns.Creational.Builder;
+using NUnit.Framework;
+
+namespace DesignPatterns.UnitTests.Creational.Builder
+{
+    [TestFixture]
+    public class BuilderTests
+    {
+        [Test]
+        public void BuilderTest()
+        {
+            var waiter = new Waiter();
+
+            waiter.SetPizzaBuilder(new HawaiianPizzaBuilder());
+            waiter.ConstructPizza();
+            var pizza = waiter.GetPizza();
+
+            Assert.That(pizza.Dough, Is.EqualTo("cross"));
+            Assert.That(pizza.Sauce, Is.EqualTo("mild"));
+            Assert.That(pizza.Topping, Is.EqualTo("ham+pineapple"));
+
+            waiter.SetPizzaBuilder(new SpicyPizzaBuilder());
+            waiter.ConstructPizza();
+            pizza = waiter.GetPizza();
+
+            Assert.That(pizza.Dough, Is.EqualTo("pan baked"));
+            Assert.That(pizza.Sauce, Is.EqualTo("hot"));
+            Assert.That(pizza.Topping, Is.EqualTo("pepperoni+salami"));
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Builder/Pizza.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.Creational.Builder
+{
+    public class Pizza
+    {
+        public Pizza()
+        {
+            Dough = string.Empty;
+            Sauce = string.Empty;
+            Topping = string.Empty;
+        }
+
+        public string Dough { get; private set; }
+
+        public string Sauce { get; private set; }
+
+        public string Topping { get; private set; }
+
+        public void SetDough(string dough)
+        {
+            Dough = dough;
+        }
+
+        public void SetSauce(string sauce)
+        {
+            Sauce = sauce;
+        }
+
+        public void SetTopping(string topping)
+        {
+            Topping = topping;
+        }
+    }
+}
