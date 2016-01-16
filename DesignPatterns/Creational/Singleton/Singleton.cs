47cfdfc82396462679c15ca48b898e18bbccf743
Ahmed Agabani <agabani@live.com>
2016-01-16 00:58:52 +0000

    Singleton

---
+++ DesignPatterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _singleton;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Accessor()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new Singleton();
+            }
+
+            return _singleton;
+        }
+
+        public static bool IsInitialized()
+        {
+            return _singleton != null;
+        }
+    }
+}
