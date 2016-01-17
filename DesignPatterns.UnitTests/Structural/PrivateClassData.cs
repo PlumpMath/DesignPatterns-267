4361e8a21358060e135f0b86421fc23c74545e90
Ahmed Agabani <agabani@live.com>
2016-01-17 15:55:48 +0000

    PrivateClassData

---
+++ DesignPatterns.UnitTests/Structural/PrivateClassData.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Structural.PrivateClassData;
+using NUnit.Framework;
+
+namespace DesignPatterns.UnitTests.Structural
+{
+    [TestFixture]
+    public class PrivateClassData
+    {
+        [Test]
+        public void Test()
+        {
+            var @class = new Class();
+            
+            Assert.That(@class.Data.Property1, Is.EqualTo("property 1"));
+            Assert.That(@class.Data.Property2, Is.EqualTo("property 2"));
+
+            @class.ModifyPrivateData("new property");
+
+            Assert.That(@class.Data.Property1, Is.EqualTo("property 1"));
+            Assert.That(@class.Data.Property2, Is.EqualTo("new property"));
+
+        }
+    }
+}
---
+++ DesignPatterns/Structural/PrivateClassData/PrivateClassData.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.Structural.PrivateClassData
+{
+    public class Class
+    {
+        public Class()
+        {
+            Data = new PrivateData("property 1", "property 2");
+        }
+
+        public PrivateData Data { get; }
+
+        public void ModifyPrivateData(string property)
+        {
+            Data.SetProperty2(property);
+        }
+    }
+
+    public class PrivateData
+    {
+        public PrivateData(string property1, string property2)
+        {
+            Property1 = property1;
+            Property2 = property2;
+        }
+
+        public string Property1 { get; private set; }
+        public string Property2 { get; private set; }
+
+        public void SetProperty2(string property)
+        {
+            Property2 = property;
+        }
+    }
+}
