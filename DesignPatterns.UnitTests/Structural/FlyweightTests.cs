ccc04832db4a972db8bcbfb828a51dda682ddfad
Ahmed Agabani <agabani@live.com>
2016-01-17 15:42:51 +0000

    Flyweight

---
+++ DesignPatterns.UnitTests/Structural/FlyweightTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DesignPatterns.Structural.Flyweight;
+using NUnit.Framework;
+using NUnit.Framework.Compatibility;
+
+namespace DesignPatterns.UnitTests.Structural
+{
+    [TestFixture]
+    public class FlyweightTests
+    {
+        [Test]
+        public void Test()
+        {
+            var flyweightFactory = new FlyweightFactory();
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            var flyweight1 = flyweightFactory.Get(TimeSpan.FromMilliseconds(200));
+            stopwatch.Stop();
+
+            Assert.That(flyweight1, Is.Not.Null);
+            Assert.That(stopwatch.Elapsed, Is
+                .GreaterThan(TimeSpan.FromMilliseconds(100))
+                .And
+                .LessThan(TimeSpan.FromMilliseconds(300)));
+
+            stopwatch.Restart();
+            var flyweight2 = flyweightFactory.Get(TimeSpan.FromMilliseconds(200));
+            stopwatch.Stop();
+
+            Assert.That(flyweight2, Is.Not.Null);
+            Assert.That(stopwatch.Elapsed, Is
+                .LessThan(TimeSpan.FromMilliseconds(1)));
+
+            Assert.That(flyweight1, Is.SameAs(flyweight2));
+            Assert.That(flyweight1.ShareableState, Is.SameAs(flyweight2.ShareableState));
+
+            var flyweight3 = flyweightFactory.Get(TimeSpan.Zero);
+
+            Assert.That(flyweight3, Is.Not.Null);
+            Assert.That(flyweight3, Is.Not.SameAs(flyweight1));
+
+            var response1 = flyweight1.Method(new Flyweight.ExtrinsicState {Content = "content 1"});
+            var response2 = flyweight1.Method(new Flyweight.ExtrinsicState {Content = "content 2"});
+
+            Assert.That(response1, Is.EqualTo("[00:00:00.2000000] content 1"));
+            Assert.That(response2, Is.EqualTo("[00:00:00.2000000] content 2"));
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Flyweight/Flyweight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DesignPatterns.Structural.Flyweight
+{
+    public class FlyweightFactory
+    {
+        private readonly Dictionary<TimeSpan, Flyweight> _flyweights;
+
+        public FlyweightFactory()
+        {
+            _flyweights = new Dictionary<TimeSpan, Flyweight>();
+        }
+
+        public Flyweight Get(TimeSpan timeSpan)
+        {
+            if (_flyweights.ContainsKey(timeSpan))
+            {
+                return _flyweights[timeSpan];
+            }
+
+            var flyweight = new Flyweight(timeSpan);
+            _flyweights.Add(timeSpan, flyweight);
+            return flyweight;
+        }
+    }
+
+    public class Flyweight
+    {
+        public Flyweight(TimeSpan timeSpan)
+        {
+            ShareableState = new IntrinsicState
+            {
+                InitializationTime = timeSpan
+            };
+            Thread.Sleep(timeSpan);
+        }
+
+        public IntrinsicState ShareableState { get; }
+
+        public string Method(ExtrinsicState nonsharableState)
+        {
+            return $"[{ShareableState.InitializationTime}] {nonsharableState.Content}";
+        }
+
+        public class ExtrinsicState
+        {
+            public string Content { get; set; }
+        }
+
+        public class IntrinsicState
+        {
+            public TimeSpan InitializationTime { get; set; }
+        }
+    }
+}
