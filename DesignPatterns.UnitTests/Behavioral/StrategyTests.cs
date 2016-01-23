9d90420e055403b0bdbdc693059f3a4a223d9dd9
Ahmed Agabani <agabani@live.com>
2016-01-23 22:34:32 +0000

    Strategy

---
+++ DesignPatterns.UnitTests/Behavioral/StrategyTests.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.Behavioral.Strategy;
+using NUnit.Framework;
+
+namespace DesignPatterns.UnitTests.Behavioral
+{
+    [TestFixture]
+    public class StrategyTests
+    {
+        [Test]
+        public void Test()
+        {
+            Strategy strategy = new PowerStrategy();
+            Assert.That(strategy.Solve(27,3), Is.EqualTo(19683));
+
+            strategy = new RootStrategy();
+            Assert.That(strategy.Solve(27, 3), Is.EqualTo(3));
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Strategy/Strategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+    public abstract class Strategy
+    {
+        public abstract double Solve(double a, double b);
+    }
+
+    public class PowerStrategy : Strategy
+    {
+        public override double Solve(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+
+    public class RootStrategy : Strategy
+    {
+        public override double Solve(double a, double b)
+        {
+            return Math.Pow(a, 1/b);
+        }
+    }
+}
