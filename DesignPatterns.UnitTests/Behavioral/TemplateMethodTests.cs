f2102c1d514b53642b1ee640f98ae6d123e441bc
Ahmed Agabani <agabani@live.com>
2016-01-23 22:44:41 +0000

    Template Method

---
+++ DesignPatterns.UnitTests/Behavioral/TemplateMethodTests.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.Behavioral.TemplateMethod;
+using NUnit.Framework;
+
+namespace DesignPatterns.UnitTests.Behavioral
+{
+    [TestFixture]
+    public class TemplateMethodTests
+    {
+        [Test]
+        public void Test()
+        {
+            TemplateMethod templateMethod = new LowerCaseTemplateMethod();
+            Assert.That(templateMethod.Format("AbcDe"), Is.EqualTo("[abcde]"));
+
+            templateMethod = new UpperCaseTemplateMethod();
+            Assert.That(templateMethod.Format("AbcDe"), Is.EqualTo("[ABCDE]"));
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    public abstract class TemplateMethod
+    {
+        public string Format(string text)
+        {
+            var step1 = AddBrackets(text);
+            var step2 = AdjustCase(step1);
+            return step2;
+        }
+
+        private string AddBrackets(string text)
+        {
+            return $"[{text}]";
+        }
+
+        protected abstract string AdjustCase(string text);
+    }
+
+    public class LowerCaseTemplateMethod : TemplateMethod
+    {
+        protected override string AdjustCase(string text)
+        {
+            return text.ToLower();
+        }
+    }
+
+    public class UpperCaseTemplateMethod : TemplateMethod
+    {
+        protected override string AdjustCase(string text)
+        {
+            return text.ToUpper();
+        }
+    }
+}
